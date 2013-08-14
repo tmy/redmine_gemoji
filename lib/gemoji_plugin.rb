f53dd2fc17ab15797acd5d56ea649b0aefbd5e0e
Kota Shiratsuka <kota@insaneworks.co.jp>
2013-08-14 21:59:21 +0900

    Fixed emoji in Email notifications.

--- lib/gemoji_plugin.rb
+++ lib/gemoji_plugin.rb
@@ -16,7 +16,7 @@ module GemojiPlugin
     def inline_gemoji(text)
       text.gsub!(/:([a-z0-9\+\-_]+):/) do |match|
         if Emoji.names.include?($1)
-          '<img alt="' + $1 + '" height="20" src="'+ Redmine::Utils.relative_url_root + '/images/emoji/' + $1 + '.png" style="vertical-align:middle" width="20" title=":' + $1 + ':" />'
+          '<img alt="' + $1 + '" height="20" src="'+ Setting.protocol + "://" + Setting.host_name + Redmine::Utils.relative_url_root + '/images/emoji/' + $1 + '.png" style="vertical-align:middle" width="20" title=":' + $1 + ':" />'
         else
           match
         end
