5a990f0fbaa5aa3c0466c19350a9af616829d020
Tobias Fischer <tobias@mediaversal.de>
2014-02-03 12:36:38 +0100

    bump version number to 0.0.2 for latest fixes
    Includes
    - class attribute for img element
    - Fixed emoji in Email notifications

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :gemoji do
   name 'Gemoji plugin'
   author 'Akinori Tomita'
   description 'Enable github style emoji'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/tmy/redmine_gemoji'
   author_url 'https://github.com/tmy'
 end
